d471e1d077f1d124ac32180f00ad282ab96944ec
wwz2017 <32548017+wwz2017@users.noreply.github.com>
2025-05-22 00:25:35 +0800

    test:add pybi.select testing (#6)

--- __tests/testing_web/test_select.py
+++ __tests/testing_web/test_select.py
@@ -21,5 +21,23 @@ def test_options(context: Context):
     select.should_options_have_count(2)
     select.should_options_have_text("foo", "bar")
 
+def test_no_option_selected(context: Context):
+    data = {"Name": ['foo', 'bar']}
+    dataset = pybi.duckdb.from_pandas({"df": pd.DataFrame(data)})
 
+    @context.register_page
+    def index():
+        table = dataset["df"]
+        dv = pybi.data_view(f"SELECT * FROM {table}")
 
+        @ui.computed(inputs=[dv["Name"]])
+        def result(names):
+            return str(names)
+
+        pybi.select(dv["Name"])
+        pybi.label(result)
+
+    context.open()
+    select = Select(context)
+    select.should_not_selected_any()
+    context.should_see("['foo', 'bar']", equal_to=True)
