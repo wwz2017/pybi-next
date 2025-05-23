cb6bf2b87946109fb786881c10053564c2266ee2
Wwz <752157577@qq.com>
2025-05-23 07:50:32 +0000

    testSelectionImpact

--- __tests/testing_web/test_select.py
+++ __tests/testing_web/test_select.py
@@ -41,3 +41,26 @@ def test_no_option_selected(context: Context):
     select = Select(context)
     select.should_not_selected_any()
     context.should_see("['foo', 'bar']", equal_to=True)
+
+def test_selection_impact(context: Context):
+    data = {"Name":['foo','bar']}
+    dataset = pybi.duckdb.from_pandas({"df": pd.DataFrame(data)})
+
+    @context.register_page
+    def index():
+        table = dataset["df"]
+        dv = pybi.data_view(f"SELECT * FROM {table}")
+
+        @ui.computed(inputs=[dv["Name"]])
+        def result(names):
+            return str(names)
+
+        pybi.select(dv["Name"])
+        pybi.label(result)
+
+    context.open()
+    select = Select(context)
+    select.select_item("foo")
+    context.should_see("['foo']", equal_to=True)
+    
+
